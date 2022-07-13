946da679f97922773915a0e9da603b6a6f00d24a
Jayson Prince Diaz <jaysondiaz01@gmail.com>
2022-07-13 21:37:01 +0800

    widget

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./component/header/Header";
 import SideBar from "./component/sidebar/SideBar";
 import Feed from "./component/feeds/Feed";
+import Widgets from "./component/widget/Widgets";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="app__body">
         <SideBar />
         <Feed />
+        <Widgets />
       </div>
     </div>
   );
---
+++ src/component/widget/Widgets.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Widgets = () => {
+  return (
+    <div className="widget">
+      <iframe
+        src="https://www.tiktok.com/@jaysonprincediazz"
+        title="dev"
+        width="340"
+        height="100%"
+        style={{ border: "none", overflow: "hidden" }}
+        scrolling="no"
+        frameBorder="0"
+        allowTransparency="true"
+        allow="encrypted-media"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Widgets;
